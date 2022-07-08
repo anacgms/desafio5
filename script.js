f1758d308ce9f749c6098fdc0c30ffa53a9c9718
anacgms <acgomes43@gmail.com>
2022-07-08 17:51:24 -0300

    desafio5

---
+++ script.js
@@ -0,0 +1,19 @@
+let friends = ["Ross", "Monica", "Chandler", "Rachel", "Phoebe"]
+console.log(friends)
+
+friends.unshift("Joey")
+console.log(friends)
+
+friends.pop(1)
+console.log(friends)
+
+friends.push("Janet", "Carol")
+console.log(friends)
+
+friends.shift(0)
+console.log(friends)
+
+let num = [7,5,6,3,8,9,2,1,4]
+
+num.sort()
+console.log(num)
